1af821202918f9019947eb8ffb366cb3320aba23
Charles Couillard <charles@benderlabs.io>
2021-01-20 15:31:16 +0100

    feat: read quorum contract state (wip)

--- Indexer.Core/Infrastructure/BCD/BcdClient.fs
+++ Indexer.Core/Infrastructure/BCD/BcdClient.fs
@@ -9,6 +9,11 @@ type NetworkState = {
     Timestamp: DateTime
 }
 
+type QuorumOwner = {
+    IPNSPeerId: string
+    PublicKey: string
+}
+
 type BcdClient(baseUrl:string, network:string) =
     
     let client =
@@ -30,4 +35,19 @@ type BcdClient(baseUrl:string, network:string) =
             Hash = stat.Hash
             Timestamp = stat.Timestamp.Date
         }
+    }
+    
+    member this.GetQuorumOwners(address: string, level: uint64) = async {
+        let! response = client.GetAsync(sprintf "/v1/contract/%s/%s/storage?level=%i" network address level ) |> Async.AwaitTask
+        if not response.IsSuccessStatusCode then failwith "Bad"
+        let! content = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+        let value = StorageResponse.Parse(content)
+        let signers =
+                      value.Children
+                      |> Array.find(fun e -> e.Name = "signers")
+        return signers.Children
+                |> Array.map(fun e -> {
+                    IPNSPeerId = e.Name
+                    PublicKey = e.Value
+                })              
     }
--- Indexer.Test/BcdClient.Test.fs
+++ Indexer.Test/BcdClient.Test.fs
@@ -15,3 +15,18 @@ let ``Should give network state`` () =
 
         state |> should not' (equal null)
     }
+
+[<Fact>]
+let ``Should give quorum contract owners`` () =
+    async {
+        let client =
+            BcdClient("https://api.better-call.dev", "delphinet")
+
+        let! owners = client.GetQuorumOwners("KT1GuAQRrWVdwaTpURLutxGFEXHfFmBB8r5m", 375151UL)
+
+        owners.Length |> should equal 1
+        owners.[0] |> should equal {
+            IPNSPeerId = "0024080112203b7e495ee69372f4705df0c93a9b7731e6caa7208a7352b4d34566274ea15d69"
+            PublicKey = "sppk7a8xPov96ZwVh7mKi6nkkQS8r8ycYHDp7YahhnF3q1Xb3AQmBpL"
+        }
+    }
