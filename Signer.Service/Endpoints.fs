4cc6daa1480a7fa8ee1b2222d185065e8040aba8
Jean-Baptiste Dusseaut <jb@benderlabs.io>
2021-02-09 10:50:17 +0100

    feat: add admin http endpoint
    For now, it exposes only current polling status, and public keys in use.

---
+++ Signer.Service/Endpoints.fs
@@ -0,0 +1,58 @@
+module Signer.Endpoints
+
+open Giraffe
+open Giraffe.EndpointRouting
+open Signer
+open Signer.EventStore
+open Signer.IPFS
+open Signer.State.LiteDB
+open FSharp.Control.Tasks
+
+
+let statusHandler: HttpHandler =
+    handleContext (fun ctx ->
+        let s = ctx.GetService<StateLiteDb>()
+        let eventStore = ctx.GetService<EventStoreState>()
+
+        let head =
+            Cid.value (defaultArg (eventStore.GetHead()) (Cid "None"))
+
+        let eth =
+            (defaultArg (s.GetEthereumLevel()) 0I).ToString()
+
+        let tezos =
+            (defaultArg (s.GetTezosLevel()) 0I).ToString()
+
+        ctx.WriteJsonAsync
+            ({| head = head
+                tezosLevel = tezos
+                ethereumLevel = eth |})
+
+        )
+
+let keysHandler: HttpHandler =
+    handleContext (fun ctx ->
+        task {
+            let tezSigner = ctx.GetService<TezosSigner>()
+            let ethSigner = ctx.GetService<EthereumSigner>()
+
+            let! tezosKey =
+                tezSigner.PublicAddress()
+                |> AsyncResult.map (fun e -> e.GetBase58())
+
+            let! ethKey = ethSigner.PublicAddress()
+
+
+
+            let p =
+                match tezosKey, ethKey with
+                | Ok t, Ok e -> {| TezosKey = t; EthereumKey = e |}
+                | _, _ -> failwith "Error retrieving keys"
+
+            return! ctx.WriteJsonAsync p
+        })
+
+
+let endpoints =
+    [ GET [ route "status" (statusHandler)
+            route "keys" keysHandler ] ]
--- Signer.Service/Program.fs
+++ Signer.Service/Program.fs
@@ -2,36 +2,56 @@ namespace Signer
 
 open Microsoft.Extensions.FileProviders
 open Microsoft.Extensions.Hosting
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Builder
 open Signer.Configuration
 open Signer.Worker.Minting
 open Signer.Worker.Publish
 open Signer.Worker.Unwrap
 open Signer.Service
+open Signer.Endpoints
 open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+open Giraffe.EndpointRouting
 
 module Program =
 
+    let configureApp (config: IConfigurationBuilder) =
+        let configFile =
+            System.Environment.GetEnvironmentVariable("CONFIG_FILE")
+
+        let configBaseDir =
+            System.Environment.GetEnvironmentVariable("CONFIG_BASE_DIR")
+
+        if not (System.String.IsNullOrEmpty(configFile)) then
+            if not (System.String.IsNullOrEmpty(configBaseDir)) then
+                config.AddJsonFile(new PhysicalFileProvider(configBaseDir), configFile, false, false)
+                |> ignore
+            else
+                config.AddJsonFile(configFile, optional = false)
+                |> ignore
+
+    let configureServices (hostContext: WebHostBuilderContext) (services: IServiceCollection) =
+                    services
+                        .AddCommonServices(hostContext.Configuration)
+                        .AddPublisher()
+                        .AddMinter(hostContext.Configuration)
+                        .AddUnwrap(hostContext.Configuration)
+                        .AddSigner(hostContext.Configuration)
+                        .AddRouting()
+                        .AddGiraffe()
+                    |> ignore
+    
     let createHostBuilder args =
         Host
             .CreateDefaultBuilder(args)
-            .ConfigureAppConfiguration(fun (hostingContext: HostBuilderContext) (config: IConfigurationBuilder) ->
-                let configFile = System.Environment.GetEnvironmentVariable("CONFIG_FILE")
-                let configBaseDir = System.Environment.GetEnvironmentVariable("CONFIG_BASE_DIR")
-                if not (System.String.IsNullOrEmpty(configFile)) then
-                    if not(System.String.IsNullOrEmpty(configBaseDir)) then
-                        config.AddJsonFile( new PhysicalFileProvider(configBaseDir), configFile, false, false) |> ignore
-                    else
-                        config.AddJsonFile(configFile, optional = false) |> ignore
-                )
-            .ConfigureServices(fun hostContext services ->
-                services
-                    .AddCommonServices(hostContext.Configuration)
-                    .AddPublisher()
-                    .AddMinter(hostContext.Configuration)
-                    .AddUnwrap(hostContext.Configuration)
-                    .AddSigner(hostContext.Configuration)
+            .ConfigureWebHostDefaults(fun webHostBuilder ->
+                webHostBuilder
+                    .ConfigureAppConfiguration(configureApp)
+                    .ConfigureServices(configureServices)
+                    .Configure(fun app -> app.UseRouting().UseGiraffe(endpoints) |> ignore)
                 |> ignore)
-
 
     [<EntryPoint>]
     let main args =
